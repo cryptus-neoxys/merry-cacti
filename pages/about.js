2529096f7c95fafe6d7fc076da0514406f1cc09a
Vinayak Gupta <guptavinayakg@gmail.com>
2020-12-04 23:27:39 +0530

    about page

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import styles from "../styles/About.module.css";
+
+export default function About() {
+  return (
+    <div className={styles.container}>
+      <img
+        src="https://i.gifer.com/Vr6.gif"
+        alt="bg"
+        className={styles.bgImg}
+      />
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,8 @@
-import Head from "next/head";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <img
-        src="https://i.gifer.com/Vr6.gif"
-        alt="bg"
-        className={styles.bgImg}
-      />
     </div>
   );
 }
