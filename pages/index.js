6634ebf294c0fd52b149c0934ebbf3d15d3b3d15
cryptus-neoxys <sharma.dev4242@gmail.com>
2020-12-04 22:57:53 +0530

    minor fix

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,6 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  return (
-    <div className="container">
-
-    </div>
-  )
+  return <div className={styles.container}></div>;
 }
