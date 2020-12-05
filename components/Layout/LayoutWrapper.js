ccdd50e8a01bf0f0c08695c6372dc54ab6fb3846
cryptus-neoxys <sharma.dev4242@gmail.com>
2020-12-05 20:30:41 +0530

    Gloabl styles in Layout

--- components/Layout/LayoutWrapper.js
+++ components/Layout/LayoutWrapper.js
@@ -1,3 +1,15 @@
 import styled from "styled-components";
 
-export const LayoutWrapper = styled.div``;
+export const LayoutWrapper = styled.div`
+  padding: 0;
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+    Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  * {
+    box-sizing: border-box;
+  }
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+`;
