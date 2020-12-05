ee61c08168e43beb84d0f36a508e2ca9ae19aadc
cryptus-neoxys <sharma.dev4242@gmail.com>
2020-12-05 16:27:20 +0530

    minor fix

--- components/NavBar.js
+++ components/NavBar.js
@@ -4,14 +4,22 @@ import styles from "../styles/NavBar.module.css";
 const NavBar = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.navLogo}><Link href="/">Reddit Bot</Link></div>
+      <div className={styles.navLogo}>
+        <Link href="/">Reddit Bot</Link>
+      </div>
       <div className={styles.navItems}>
-        <div className={styles.navItem}><Link href="/about">About</Link></div>
-        <div className={styles.navItem}><Link href="/pricing">Pricing</Link></div>
-        <div className={styles.navItem}><Link href="/contact">Contact</Link></div>
+        <div className={styles.navItem}>
+          <Link href="/about">About</Link>
+        </div>
+        <div className={styles.navItem}>
+          <Link href="/pricing">Pricing</Link>
+        </div>
+        <div className={styles.navItem}>
+          <Link href="/contact">Contact</Link>
+        </div>
       </div>
     </div>
   );
 };
-<Link href="/about"></Link>
-export default NavBar;<Link href="/about">About</Link>
+
+export default NavBar;
