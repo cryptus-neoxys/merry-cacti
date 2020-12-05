4b00b38569dbb2530eeab16ae1a830170d1d036a
Aniket Singh Rawat <sprx077@gmail.com>
2020-12-05 21:10:38 +0530

    done team

--- styles/TeamWrapper.js
+++ styles/TeamWrapper.js
@@ -1,3 +1,30 @@
 import styled from "styled-components";
 
-export const TeamWrapper = styled.div``;
+export const TeamWrapper = styled.div`
+  .heading {
+    font-size: 40px;
+    text-align: center;
+    margin-top: 100px;
+  }
+  .team-group {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: center;
+
+    .team-member {
+      margin: 2em 2em;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      .member-name {
+        font-size: 20px;
+        margin: 20px 0px;
+      }
+      .member-image {
+        width: 200px;
+        border: 2px solid #eeeeee;
+      }
+    }
+  }
+`;
